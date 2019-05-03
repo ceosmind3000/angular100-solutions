4915a6cd26fb29130b68705be8303c1578e63b74
cioffi42 <cioffi42@gmail.com>
2019-05-03 13:39:25 -0400

    format and json updates

--- src/app/albums/album-details/album-details.component.ts
+++ src/app/albums/album-details/album-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { AlbumService } from "../album.service";
+
+import { AlbumService } from "../shared/album.service";
 import { Album } from "../album.model";
 
 @Component({
--- src/app/albums/shared/album.service.ts
+++ src/app/albums/shared/album.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+
 import { Observable } from "rxjs";
-import { Album } from "./album.model";
+import { Album } from "../album.model";
 
 @Injectable({
   providedIn: "root"
